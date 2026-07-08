8073c8671e0f16b96959ef25b2a85e459e0abf74
oscar.yuste <inane.ensemble@gmail.com>
2026-07-08 12:47:41 +0200

    feat(source-ingestion): provide fake ingestion gateway in app config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,16 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { SOURCE_INGESTION_GATEWAY } from './source-ingestion/application/ports/SourceIngestionGateway';
+import { FakeSourceIngestionAdapter } from './source-ingestion/infrastructure/adapters/FakeSourceIngestionAdapter';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+    provideHttpClient(),
+    provideRouter(routes),
+    { provide: SOURCE_INGESTION_GATEWAY, useClass: FakeSourceIngestionAdapter },
+  ],
 };
